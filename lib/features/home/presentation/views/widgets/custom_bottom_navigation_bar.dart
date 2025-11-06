75c69d465245768118b30567c707566541bae3e4
tasneemkhaled <khaledtasneem402@gmail.com>
2025-11-06 21:18:56 +0200

    edit custom_nav_bar

--- lib/features/home/presentation/views/widgets/custom_bottom_navigation_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_bottom_navigation_bar.dart
@@ -1,3 +1,40 @@
+// // import 'package:flutter/material.dart';
+// import 'package:fruit_app/core/utils/constants.dart';
+
+// class CustomBottomNavigationBar extends StatelessWidget {
+//   const CustomBottomNavigationBar({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return BottomNavigationBar(
+//       showSelectedLabels: true,
+//       showUnselectedLabels: false,
+//       backgroundColor: pColor,
+//       type: BottomNavigationBarType.fixed,
+//       items: const [
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.home, color: Colors.white),
+//           label: 'Home',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.format_list_bulleted, color: Colors.white),
+//           label: 'List',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.shopping_cart, color: Colors.white),
+//           label: 'Cart',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.favorite_border, color: Colors.white),
+//           label: 'Favorites',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.menu, color: Colors.white),
+//           label: 'Menu',
+//         ),
+//       ],
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:fruit_app/core/utils/constants.dart';
 
